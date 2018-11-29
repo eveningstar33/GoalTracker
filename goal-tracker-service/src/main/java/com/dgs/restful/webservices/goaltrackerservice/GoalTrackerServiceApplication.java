5cef0d1720bbb62ede5b6760a14c72f3a055f468
Dragos Sarpe <contact@usdgadget.com>
2018-11-29 22:22:29 +0200

    Initial import of Goal RESTful API (backend)

---
+++ goal-tracker-service/src/main/java/com/dgs/restful/webservices/goaltrackerservice/GoalTrackerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.restful.webservices.goaltrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalTrackerServiceApplication.class, args);
+	}
+}
