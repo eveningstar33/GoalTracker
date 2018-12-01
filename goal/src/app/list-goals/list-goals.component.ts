bd0c30b8c455f09fe983e1efe323c3dc64e729dc
Dragos Sarpe <contact@usdgadget.com>
2018-12-01 18:05:33 +0200

    Added a Goal class

--- goal/src/app/list-goals/list-goals.component.ts
+++ goal/src/app/list-goals/list-goals.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { detachEmbeddedView } from '@angular/core/src/view';
+
+export class Goal {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
 
 @Component({
   selector: 'app-list-goals',
@@ -8,19 +20,23 @@ import { Component, OnInit } from '@angular/core';
 export class ListGoalsComponent implements OnInit {
 
   goals = [
-    {
-      id: 1,
-      description: 'Learn to Dance'
-    },
-    {
-      id: 2,
-      description: 'Visit USA'
-    },
-    {
-      id: 3,
-      description: 'Become an Expert at Angular'
-    }
-  ]; 
+    new Goal(1, 'Learn to Dance', false, new Date()),
+    new Goal(2, 'Visit USA', false, new Date()),
+    new Goal(3, 'Become an Expert at Angular', false, new Date())
+    
+    // {
+    //   id: 1,
+    //   description: 'Learn to Dance'
+    // },
+    // {
+    //   id: 2,
+    //   description: 'Visit USA'
+    // },
+    // {
+    //   id: 3,
+    //   description: 'Become an Expert at Angular'
+    // }
+  ];
 
   constructor() { }
 
