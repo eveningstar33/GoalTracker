c1c169c35dc46bc3ddfe953e112fd4376472e3ef
Dragos Sarpe <contact@usdgadget.com>
2018-12-01 14:41:32 +0200

    Added interpolation, 2 ways data binding and Angular directives

--- goal/src/app/login/login.component.ts
+++ goal/src/app/login/login.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'dgs';
+  password = '';
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLogin() {
+    // console.log(`The username is: ${this.username}`); 
+    if (this.username === 'dgs' && this.password === 'test') {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
   }
 
 }
