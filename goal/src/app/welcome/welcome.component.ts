0a967a1c00cc19309496a25348e4cb6fa7859c3c
Dragos Sarpe <contact@usdgadget.com>
2018-12-19 12:47:38 +0200

    Added data service

--- goal/src/app/welcome/welcome.component.ts
+++ goal/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -11,11 +12,18 @@ export class WelcomeComponent implements OnInit {
   name = '';
 
   // Inject ActivatedRoute dependency
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) { }
 
   ngOnInit() {
     // We need to pick up the route parameter name - params is a Map, name is the Key
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage() {
+    this.service.executeHelloWorldBeanService();
+    // console.log('Get welcome message');
   }
 
 }
