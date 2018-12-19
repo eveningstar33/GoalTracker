23387dd5c7a588926e06b7898d1a621005dbd55c
Dragos Sarpe <contact@usdgadget.com>
2018-12-19 17:46:10 +0200

    Added another REST service for testing

--- goal-tracker-service/src/main/java/com/dgs/restful/webservices/goaltrackerservice/helloworld/HelloWorldController.java
+++ goal-tracker-service/src/main/java/com/dgs/restful/webservices/goaltrackerservice/helloworld/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.dgs.restful.webservices.goaltrackerservice.helloworld;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")          // Allow requests from other domains
 @RestController
 public class HelloWorldController {
 
@@ -13,6 +16,12 @@ public class HelloWorldController {
 	
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
+		// throw new RuntimeException("Some Error has Happend! Contact Support at *** - ****");
 		return new HelloWorldBean("Hello World - Changed");
+	}
+	
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
