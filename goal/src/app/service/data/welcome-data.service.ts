a1f582ff0a434d60fae562ed9718d0393c5a0c02
Dragos Sarpe <contact@usdgadget.com>
2018-12-19 17:44:07 +0200

    Calling welcome HTTP service with path variable

--- goal/src/app/service/data/welcome-data.service.ts
+++ goal/src/app/service/data/welcome-data.service.ts
@@ -17,4 +17,8 @@ export class WelcomeDataService {
     // console.log("Execute Hello World Bean Service");
   }
 
+  executeHelloWorldBeanServiceWithPathVariable(name) {
+    return this.http.get<HelloWorldBean>(`http://localhost:8080//hello-world/path-variable/${name}`);
+  }
+
 }
--- goal/src/app/welcome/welcome.component.ts
+++ goal/src/app/welcome/welcome.component.ts
@@ -23,18 +23,36 @@ export class WelcomeComponent implements OnInit {
 
   getWelcomeMessage() {
     // console.log('Get welcome message');
-    console.log(this.service.executeHelloWorldBeanService());  // We'll get an Observable in the console
+    // console.log(this.service.executeHelloWorldBeanService());  // We'll get an Observable in the console
 
     // To execute the request we need to subscribe to the observable
     this.service.executeHelloWorldBeanService().subscribe(
-      response => {
-        // console.log(response);
-        // console.log(response.message);
-        this.welcomeMessageFromService = response.message;
-      },
-      error => {
-        this.welcomeMessageFromService = error.error.message;
-      }
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+
+    // console.log('last line of getWelcomeMessage()');
+
+  }
+
+  handleSuccessfulResponse(response) {
+    // console.log(response);
+    // console.log(response.message);
+    this.welcomeMessageFromService = response.message;
+  }
+
+  handleErrorResponse(error) {
+    this.welcomeMessageFromService = error.error.message;
+  }
+
+  getWelcomeMessageWithParameter() {
+    // console.log('Get welcome message');
+    // console.log(this.service.executeHelloWorldBeanService());  // We'll get an Observable in the console
+
+    // To execute the request we need to subscribe to the observable
+    this.service.executeHelloWorldBeanServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
     );
 
     console.log('last line of getWelcomeMessage()');
