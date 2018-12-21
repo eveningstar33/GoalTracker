c75c9583ae92fed151987a3ef6fb90d0a1c98674
Dragos Sarpe <contact@usdgadget.com>
2018-12-21 10:33:55 +0200

    Added Angular Basic Authentication service

---
+++ goal/src/app/service/basic-authentication.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+    constructor(private http: HttpClient) { }
+
+    executeAuthenticationService(username, password) {
+
+        let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+        let headers = new HttpHeaders({
+            Authorization: basicAuthHeaderString
+        });
+        
+        return this.http.get<AuthenticationBean>(
+            'http://localhost:8080/basicauth',
+            {headers});
+    }
+}
+
+export class AuthenticationBean {
+    constructor(public message: string) { }
+}
