f37b8d51b6cd526f8b8df5da482682023ccef618
Dragos Sarpe <contact@usdgadget.com>
2018-12-21 10:11:45 +0200

    Added Basic Authentication RESTful service in Spring Boot

--- goal-tracker-service/src/main/java/com/dgs/restful/webservices/goaltrackerservice/helloworld/HelloWorldController.java
+++ goal-tracker-service/src/main/java/com/dgs/restful/webservices/goaltrackerservice/helloworld/HelloWorldController.java
@@ -22,6 +22,6 @@ public class HelloWorldController {
 	
 	@GetMapping("/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-		return new HelloWorldBean(String.format("Hello World, %s", name));
+		return new HelloWorldBean(String.format("Hello, %s", name));
 	}
 }
